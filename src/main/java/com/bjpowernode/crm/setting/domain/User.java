55ecc3b5a34bc6c41648d53805805ab2c6c48a92
gant <gmb1995@outlook.com>
2019-07-13 18:18:36 +0800

    Revert "初始化模块"
    This reverts commit 747a8ea3

---
+++ src/main/java/com/bjpowernode/crm/setting/domain/User.java
@@ -0,0 +1,160 @@
+package com.bjpowernode.crm.setting.domain;
+
+/**
+ * Author: 甘明波
+ * 2019-07-13
+ */
+public class User {
+    private String id; //32位uuid，主键
+    private String loginAct; //登陆账户
+    private String name;        //用户名
+    private String loginPwd;    //登陆密码
+    private String email;       //伊妹儿
+    private String expireTime;  //账户失效时间
+    private String lockState;   //是否锁定
+    private String deptno;      //部门编号
+    private String allowIps;    //允许的ip地址群
+    private String createTime;  //创建的时间
+    private String createBy;    //创建人
+    private String editTime;    //上一次编辑的时间
+    private String editBy;      //编辑的人
+
+    public User() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public User setId(String id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getLoginAct() {
+        return loginAct;
+    }
+
+    public User setLoginAct(String loginAct) {
+        this.loginAct = loginAct;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public User setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getLoginPwd() {
+        return loginPwd;
+    }
+
+    public User setLoginPwd(String loginPwd) {
+        this.loginPwd = loginPwd;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public User setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public String getExpireTime() {
+        return expireTime;
+    }
+
+    public User setExpireTime(String expireTime) {
+        this.expireTime = expireTime;
+        return this;
+    }
+
+    public String getLockState() {
+        return lockState;
+    }
+
+    public User setLockState(String lockState) {
+        this.lockState = lockState;
+        return this;
+    }
+
+    public String getDeptno() {
+        return deptno;
+    }
+
+    public User setDeptno(String deptno) {
+        this.deptno = deptno;
+        return this;
+    }
+
+    public String getAllowIps() {
+        return allowIps;
+    }
+
+    public User setAllowIps(String allowIps) {
+        this.allowIps = allowIps;
+        return this;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public User setCreateTime(String createTime) {
+        this.createTime = createTime;
+        return this;
+    }
+
+    public String getCreateBy() {
+        return createBy;
+    }
+
+    public User setCreateBy(String createBy) {
+        this.createBy = createBy;
+        return this;
+    }
+
+    public String getEditTime() {
+        return editTime;
+    }
+
+    public User setEditTime(String editTime) {
+        this.editTime = editTime;
+        return this;
+    }
+
+    public String getEditBy() {
+        return editBy;
+    }
+
+    public User setEditBy(String editBy) {
+        this.editBy = editBy;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", loginAct='" + loginAct + '\'' +
+                ", name='" + name + '\'' +
+                ", loginPwd='" + loginPwd + '\'' +
+                ", email='" + email + '\'' +
+                ", expireTime='" + expireTime + '\'' +
+                ", lockState='" + lockState + '\'' +
+                ", deptno='" + deptno + '\'' +
+                ", allowIps='" + allowIps + '\'' +
+                ", createTime='" + createTime + '\'' +
+                ", createBy='" + createBy + '\'' +
+                ", editTime='" + editTime + '\'' +
+                ", editBy='" + editBy + '\'' +
+                '}';
+    }
+}
